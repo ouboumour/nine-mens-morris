65277748a951c32b75bdc43f2e053c6ac39df702
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2025-01-25 23:52:11 +0100

    add init / destroy WelcomePageView to controller

--- src/controller/GameController.c
+++ src/controller/GameController.c
@@ -2,10 +2,20 @@
 
 #include "GameController.h"
 
+void initWelcomePageView() {
+    printf("initWelcomePageView...\n");
+}
+
+void destroyWelcomePageView() {
+    printf("destroyWelcomePageView...\n");
+}
+
 void startTheGame() {
     printf("Game started...\n");
+    initWelcomePageView();
 }
 
 void endTheGame() {
+    destroyWelcomePageView();
     printf("Bye...\n");
 }
